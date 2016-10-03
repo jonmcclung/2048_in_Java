2d72788cb90da08ec2515333a1396e840387d60b
Jonathan McClung <jonmcclung@gmail.com>
2016-10-03 08:36:41 -0500

    adding IDE stuff in case you want to run it in intelliJ as well as better build scripts

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 
-set path=%path%;C:\Program Files\Java\jdk1.8.0_101\bin;C:\2120\Java\ant-1.9.6
+::set path=%path%;C:\Program Files\Java\jdk1.8.0_101\bin;C:\2120\Java\ant-1.9.6\bin
 
 ::if "%2" == "test" goto test
 if "%1" == "build" goto build
@@ -13,4 +13,4 @@ goto end
 ant build
 goto end
 
-end:
+:end
